a8e0a6478813f4c9fe8751b564e7a855c4bf29ea
Will Foley <william.w.foley@gmail.com>
2021-04-28 22:47:19 -0400

    changed CDNs to local packages

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({
-        "node_modules/patternomaly/dist/patternomaly.js": "js/patternomaly.js"
+        "node_modules/patternomaly/dist/patternomaly.js": "js/patternomaly.js",
+        "node_modules/chart.js/dist/chart.min.js": "js/chart.min.js",
+        "node_modules/@fortawesome/fontawesome-free/js/all.min.js": "js/all.min.js",
+        "node_modules/@fortawesome/fontawesome-free/css/all.min.css": "css/all.min.css",
+        "node_modules/@fortawesome/fontawesome-free/webfonts": "webfonts",
+        "node_modules/bootstrap/dist/css/bootstrap.min.css": "css/bootstrap.min.css",
     });
 };
